553edc8ef7a75681924ec2f6fa13a9205e3731bb
kkasprow_blebox <krystian.kasprow@blebox.eu>
2020-03-29 11:51:30 +0200

    my4 app created succesfully

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
+// imports needed if using NPM package; not if from CDN links
+import React from "react";
+//import ReactDOM from "react-dom";
 
-ReactDOM.render()
+//const greeting = <h1>Hello React</h1>;
+
+// ReactDOM.render(root node, mounting point)
+//ReactDOM.render(greeting, document.getElementById("root"));
+
+console.log("This is my console LOG!!! NICIA u see?")
