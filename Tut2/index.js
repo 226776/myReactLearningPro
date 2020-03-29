35666a480bbac71bf3f02e12cfb980710434f16a
Diith <48160583+226776@users.noreply.github.com>
2020-03-29 21:39:04 +0200

    my5 Training App

---
+++ Tut2/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+//console.log('.js Works!');
+
+//const element = <h1>Hello World</h1>;
+
+//console.log(emelent);
---
+++ Tut5/my5/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+var myList = 
+<div>
+    <h1>My List Beyb</h1>
+    <p>List contains random szczaw</p>
+    <ul>
+        <li>Test</li>
+        <li>5</li>
+    </ul>
+</div>
+
+
+ReactDOM.render(myList, document.getElementById("root"))
