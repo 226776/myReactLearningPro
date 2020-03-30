1c606b04c6a858aafe93ddd693eb96157d5aa139
kkasprow_blebox <krystian.kasprow@blebox.eu>
2020-03-30 16:40:04 +0200

    no-create-react-app : Branch Init

--- Tut4/my4/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-    return <div>
-        <p>Footer: Nothing there yet</p>
-    </div>
-}
-
-export default Footer
--- Tut4/my4/src/components/MainContent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MainContent() {
-    return <div>
-        <p>MainContent: Nothing there yet</p>
-    </div>
-}
-
-export default MainContent
